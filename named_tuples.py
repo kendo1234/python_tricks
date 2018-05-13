e369aae288876ffffd9ad8f37d3fc3993ef16057
kendo1234 <kendomustdie@googlemail.com>
2018-05-13 17:28:01 +0100

    named tuple

---
+++ named_tuples.py
@@ -0,0 +1,18 @@
+# A tuple is a sequence of immutable Python objects. Tuples are sequences, just
+# like lists. The differences between tuples and lists are, the tuples cannot be
+# changed unlike lists and tuples use parentheses, whereas lists use square
+# brackets.
+from collections import namedtuple
+
+Car = namedtuple('Car', 'color mileage')
+
+# Our new car class works as expected
+
+my_car = Car('red', 3812.4)
+
+# Like tuples, namedtuples are immutable:
+# >>> my_car.color = 'blue'
+# AttributeError: "can't set attribute"
+
+
+print(my_car)
