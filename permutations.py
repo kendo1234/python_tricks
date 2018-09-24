347b53dad4a460c35f929863ff1c5ccea3720e58
kendo1234 <kendomustdie@googlemail.com>
2018-09-24 20:09:21 +0100

    learning exceptions with boxprint

---
+++ permutations.py
@@ -0,0 +1,5 @@
+# itertools.permutations() generates permutations
+# for an iterable. Time to brute-force those passwords ;-)
+import itertools
+for p in itertools.permutations('ABCD'):
+    print(p)
